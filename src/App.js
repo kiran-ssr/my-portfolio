65e43bee55a8aa3c3cbe45d1643f500d04ebc93a
kiran-ssr <dev.kssr@gmail.com>
2022-08-27 04:38:49 -0400

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import About from "./components/pages/About";
+import Experience from "./components/pages/Experience";
+import Contact from "./components/pages/Contact";
+import Header from "./components/ui/Header";
+import Navbar from "./components/ui/Navbar";
+import Footer from "./components/ui/Footer";
 
 function App() {
   return (
-    <div className="App">
-     <p>Start add</p>
+    <div>
+      <Header />
+      <Navbar />
+      <About />
+      <Experience />
+      <Contact />
+      <Footer />
     </div>
   );
 }

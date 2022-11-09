cc44d71ed5a824ef3861adcafb69ac6adaed60ad
kiran-ssr <dev.kssr@gmail.com>
2022-11-09 08:20:19 -0500

    NavBar -New

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./App.css";
+import "./App.scss";
+import HireMe from "./components/pages/HireMe";
+import Skills from "./components/pages/Skills";
+import Projects from "./components/pages/Projects";
+import Home from "./components/pages/Home";
+import Navbar from "./components/ui/Navbar";
 
 function App() {
   return (
-    <>
-      <main className="main"></main>
-    </>
+    <div className="app">
+      <Navbar />
+      <Home />
+      <Skills />
+      <Projects />
+      <HireMe />
+    </div>
   );
 }
 

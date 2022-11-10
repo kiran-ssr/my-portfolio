514f316899d2fefb8e1ff070cb1bde1c94cf630f
kiran-ssr <dev.kssr@gmail.com>
2022-11-09 21:26:26 -0500

    Complete Revamp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import "./App.scss";
-import HireMe from "./components/pages/HireMe";
-import Skills from "./components/pages/Skills";
-import Projects from "./components/pages/Projects";
-import Home from "./components/pages/Home";
-import Navbar from "./components/ui/Navbar";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <Navbar />
-      <Home />
-      <Skills />
-      <Projects />
-      <HireMe />
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/pages/HireMe/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HireMe = () => {
-  return <div>HireMe</div>;
-};
-
-export default HireMe;
